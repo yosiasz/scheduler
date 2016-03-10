9ab2b9da141a9babf75cc3e04b1c8c4c3e283d63
Josiah Solomon <Josiah.Solomon@corbis.com>
2016-03-10 11:16:19 -0800

    mongo implementation

--- app/config/configmongo.js
+++ app/config/configmongo.js
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 //172.16.32.62  scheduler Semrina77
 var module;
 
 module.exports = {
     'url' : 'mongodb://scheduler:Semrina77@172.16.32.62:27017/scheduler'
 };
+=======
+//172.16.32.62  Admin Semrina77
+module.exports ={
+'url' : 'mongodb://admin:password@172.16.32.62:27017/scheduler'
+};
+>>>>>>> f684ba4246d21e231066de9956575829394e51e9
